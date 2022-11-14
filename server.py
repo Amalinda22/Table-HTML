c8ea7cde04936a9d1f1caa17fcdc851d612cfeb2
Amalinda22 <115367911+Amalinda22@users.noreply.github.com>
2022-11-14 18:16:47 +0100

    hi

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template  # Import Flask to allow us to create our app
+app = Flask(__name__) 
+# users = [
+#    {'first_name' : 'Michael', 'last_name' : 'Choi'},
+#    {'first_name' : 'John', 'last_name' : 'Supsupin'},
+#    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+#     ]                                        # Create a new instance of the Flask class called "app"
+@app.route('/')                                         # The "@" decorator associates this route with the function immediately following
+def list_users():
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]    
+    return render_template("dashboard.html", students = users)   
+                        # Return the string 'Hello World!' as a response
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)
